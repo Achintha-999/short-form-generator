6344e6a24cbbed202edd5dec63e8694dcaa48acb
Achintha-999 <achinthasandaruwan517@gmail.com>
2025-12-21 15:46:29 +0530

    first commit

---
+++ short_form_generator.py
@@ -0,0 +1,11 @@
+def generator(sentence):
+	word_list = sentence.split()
+	short_form = ""
+	for i in word_list:
+		short_form = short_form + i[0].upper()
+	print(short_form)
+
+
+text = input("Enter the sentence: ")
+generator(text)
+
